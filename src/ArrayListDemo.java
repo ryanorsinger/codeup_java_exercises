3be02be0947107a696dea23333198d6148524f64
ryanorsinger <ryan.orsinger@gmail.com>
2016-11-28 12:16:41 -0600

    Add arrayList demo

---
+++ src/ArrayListDemo.java
@@ -0,0 +1,33 @@
+/**
+ * Created by Moravia on 11/28/16.
+ */
+import java.util.ArrayList;
+import java.util.Iterator;
+
+
+class Fruit {
+    String name;
+    Fruit(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
+
+public class ArrayListDemo {
+    public static void main(String[] args) {
+        Fruit aFruit = new Fruit("Orange");
+        Fruit anotherFruit = new Fruit("Apple");
+
+        ArrayList<Fruit> fruits = new ArrayList<>();
+
+        fruits.add(aFruit);
+        fruits.add(anotherFruit);
+
+        for (Fruit fruit : fruits) {
+            System.out.println(fruit.getName());
+        }
+    }
+}
